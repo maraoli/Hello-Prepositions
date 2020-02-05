620814ea4623ae941b1101553fac217b8f057853
Mara <mara.15oliveira@outlook.com>
2020-02-05 10:31:48 -0300

    funções de reenderização das questoes

--- view/new-game/script.js
+++ view/new-game/script.js
@@ -1,33 +1,32 @@
 var questao;
 var random;
 var rodada = [];
-var qdtQuestoesProntas;
-var qdtQuestoesRodada = 5;
+var qdtQuestoesExistentes;
+var qdtQuestoesParaRodada = 5;
 
 var questoes = [
-    ["Questão1","resp1","resp2"],
-    ["Questão2","resp1","resp2"],
-    ["Questão3","resp1","resp2"],
-    ["Questão4","resp1","resp2"],
-    ["Questão5","resp1","resp2"],
-    ["Questão6","resp1","resp2"],
-    ["Questão7","resp1","resp2"]
+    ["Questão1","resp1","resp2",1],
+    ["Questão2","resp1","resp2",2],
+    ["Questão3","resp1","resp2",2],
+    ["Questão4","resp1","resp2",1],
+    ["Questão5","resp1","resp2",2],
+    ["Questão6","resp1","resp2",1],
+    ["Questão7","resp1","resp2",1]
 ]
 
-qdtQuestoesProntas = questoes.length;
+qdtQuestoesExistentes = questoes.length;
 
 function getRandomIntInclusive() {
     min = 0;
-    max = qdtQuestoesProntas-1;
+    max = qdtQuestoesExistentes-1;
     random = Math.floor(Math.random() * (max - min + 1)) + min;
     return random
-  }
+}
 
  
   var i = 0;
-  while( i < qdtQuestoesRodada){
+  while( i < qdtQuestoesParaRodada){
     random = getRandomIntInclusive()
-    console.log("hI");
     for(var j = 0; j <= rodada.length ;j++){
         if(random == rodada[j]){
             break;
@@ -40,16 +39,45 @@ function getRandomIntInclusive() {
     }   
   }
 
-  for(var i = 0; i < rodada.length ;i++){
-    console.log(rodada[i]);
+for(var i = 0; i < rodada.length ;i++){
+  console.log(rodada[i]);
+}
+
+var respRodada = [];
+var intRodada = 0;
+
+function salvaRodada(intRodada, intResp){
+  console.log("aqui 0");
+  respRodada.push(intResp);
+  intRodada++
+  atualizaRodada(intRodada);
+}
+
+function reenderizaQuestao(num,intRodada){
+  console.log("aqui 1");
+  var pergunta = questoes[num][0];
+  var resposta1 = questoes[num][1];
+  var resposta2 = questoes[num][2];
+
+  questao = "<p>"+ pergunta +"</p>"
+  + '<buttom onclick="salvaRodada('+intRodada+',1)">'+ resposta1 +"</buttom>"
+  + '<buttom onclick="salvaRodada('+intRodada+',2)">'+ resposta2 +"</buttom>";
+
+  document.getElementById("questoes").innerHTML = questao;
+}
+
+function atualizaRodada(intRodada){
+  console.log("aqui 2");
+  if(intRodada < rodada.length){
+    var num = rodada[intRodada];
+    reenderizaQuestao(num,intRodada);
+  }else{
+    alert("Fim de jogo");
   }
-  
-var pergunta = questoes[random][0];
-var resposta1 = questoes[random][1];
-var resposta2 = questoes[random][2];
+}
 
-questao = "<p>"+ pergunta +"</p>"
-+ "<div>"+ resposta1 +"</div>"
-+ "<div>"+ resposta2 +"</div>";
+atualizaRodada(intRodada);
 
-document.getElementById("questoes").innerHTML = questao;
+for(var i = 0; i < respRodada.length ;i++){
+  console.log(respRodada[i]);
+}
